9cf14dcd64662b23688057adb3e3ae5e0deea7c9
hen friedmann <99195954+friedmannhen@users.noreply.github.com>
2023-08-30 20:40:50 +0300

    added scrolling for todo list

--- TodoApp/src/app/services/todo.service.ts
+++ TodoApp/src/app/services/todo.service.ts
@@ -39,62 +39,62 @@ export class TodoService {
       endDate: '12/09/2022',
       selected: false,
     },
-    {
-      title: 'Puffin, horned',
-      description: 'Fratercula corniculata',
-      isCompleted: true,
-      isArchived: false,
-      endDate: '10/06/2023',
-      selected: false,
-    },
-    {
-      title: 'Ringtail',
-      description: 'Bassariscus astutus',
-      isCompleted: true,
-      isArchived: true,
-      endDate: '15/10/2023',
-      selected: false,
-    },
-    {
-      title: 'Genet, common',
-      description: 'Genetta genetta',
-      isCompleted: true,
-      isArchived: false,
-      endDate: '15/04/2024',
-      selected: false,
-    },
-    {
-      title: 'Common raccoon',
-      description: 'Procyon lotor',
-      isCompleted: true,
-      isArchived: true,
-      endDate: '03/02/2023',
-      selected: false,
-    },
-    {
-      title: 'Black and white colobus',
-      description: 'Colobus guerza',
-      isCompleted: true,
-      isArchived: true,
-      endDate: '20/04/2024',
-      selected: false,
-    },
-    {
-      title: 'Savanna baboon',
-      description: 'Papio cynocephalus',
-      isCompleted: true,
-      isArchived: false,
-      endDate: '06/11/2022',
-      selected: false,
-    },
-    {
-      title: 'Squirrel, grey-footed',
-      description: 'Paraxerus cepapi',
-      isCompleted: false,
-      isArchived: true,
-      endDate: '22/12/2022',
-      selected: false,
-    },
+    // {
+    //   title: 'Puffin, horned',
+    //   description: 'Fratercula corniculata',
+    //   isCompleted: true,
+    //   isArchived: false,
+    //   endDate: '10/06/2023',
+    //   selected: false,
+    // },
+    // {
+    //   title: 'Ringtail',
+    //   description: 'Bassariscus astutus',
+    //   isCompleted: true,
+    //   isArchived: true,
+    //   endDate: '15/10/2023',
+    //   selected: false,
+    // },
+    // {
+    //   title: 'Genet, common',
+    //   description: 'Genetta genetta',
+    //   isCompleted: true,
+    //   isArchived: false,
+    //   endDate: '15/04/2024',
+    //   selected: false,
+    // },
+    // {
+    //   title: 'Common raccoon',
+    //   description: 'Procyon lotor',
+    //   isCompleted: true,
+    //   isArchived: true,
+    //   endDate: '03/02/2023',
+    //   selected: false,
+    // },
+    // {
+    //   title: 'Black and white colobus',
+    //   description: 'Colobus guerza',
+    //   isCompleted: true,
+    //   isArchived: true,
+    //   endDate: '20/04/2024',
+    //   selected: false,
+    // },
+    // {
+    //   title: 'Savanna baboon',
+    //   description: 'Papio cynocephalus',
+    //   isCompleted: true,
+    //   isArchived: false,
+    //   endDate: '06/11/2022',
+    //   selected: false,
+    // },
+    // {
+    //   title: 'Squirrel, grey-footed',
+    //   description: 'Paraxerus cepapi',
+    //   isCompleted: false,
+    //   isArchived: true,
+    //   endDate: '22/12/2022',
+    //   selected: false,
+    // },
   ];
   private _singleTodoSubject: BehaviorSubject<ITodo> = new BehaviorSubject(
     this.mock[0]
@@ -102,16 +102,16 @@ export class TodoService {
   private _todoSubject: BehaviorSubject<Array<ITodo>> = new BehaviorSubject(
     this.mock
   );
-  constructor() {}
+  constructor() { }
   public getTodos(): Observable<Array<ITodo>> {
     return this._todoSubject.asObservable();
   }
 
-  public getSelectedTodo():Observable<ITodo>{
+  public getSelectedTodo(): Observable<ITodo> {
     return this._singleTodoSubject.asObservable();
   }
 
-  public setSelectedTodo(todo: ITodo):void{
+  public setSelectedTodo(todo: ITodo): void {
     this._singleTodoSubject.next(todo);
   }
 }
