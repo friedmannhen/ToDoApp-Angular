911f8ca5cf7fc36f17579975e9537e9112f6fe89
Hen Friedmann <99195954+friedmannhen@users.noreply.github.com>
2024-03-12 10:04:17 +0200

    changed line-threw on completed lines
    line-threw is less visible now so the text will be more readable after completed.
    changed it to work with pseudo element ' ::after ' instead of line-threw property.
     its also takes into account theme color now

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -3,6 +3,8 @@ import { ITodo } from 'src/app/models/todo.interface';
 import { TodoService } from 'src/app/services/todo.service';
 import { EditTodoComponent } from '../edit-todo/edit-todo.component';
 import { MatDialog } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-todo',
@@ -10,6 +12,8 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./todo.component.scss'],
 })
 export class TodoComponent {
+  private subscription: Subscription = new Subscription();
+
   @Input() set todo(todo: ITodo) {
     this._todo = todo;
   }
@@ -18,9 +22,20 @@ export class TodoComponent {
     return this._todo;
   }
   constructor(private todoService: TodoService, public dialog: MatDialog) {}
+  public activetheme: string;
+
   ngOnInit(): void {
+    this.subscription.add(
+      this.todoService.getThemeType().subscribe((data) => {
+        this.activetheme = data;
+        console.log(data);
+      })
+    );
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
   public toggleCompleteTodo(): void {
     this.todo.isCompleted = !this.todo.isCompleted;
     this.todoService.updateLocalStorage();
